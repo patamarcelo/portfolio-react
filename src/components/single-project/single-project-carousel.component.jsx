1c1c708b1a0296eb36e522cbe3b34086545d5187
marcelo pata <patamarcelo@gmail.com>
2022-10-07 17:46:28 -0300

    change css

--- src/components/single-project/single-project-carousel.component.jsx
+++ src/components/single-project/single-project-carousel.component.jsx
@@ -4,7 +4,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 const SingleProjectCarousel = ({picLinks}) => {
     return (
         <div className="flex justify-center w-4/5">
-								<Carousel showArrows={true} autoPlay={true}>
+								<Carousel showArrows={true} autoPlay={true} infiniteLoop={true} >
 									{picLinks.map((link, i) => {
 										return (
 											<div key={i}>
