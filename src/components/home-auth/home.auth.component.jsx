65c59c9786d2d1631a3cb28cf0e670e03f007afb
marcelo pata <patamarcelo@gmail.com>
2022-10-16 20:09:12 -0300

    add get risks

--- src/components/home-auth/home.auth.component.jsx
+++ src/components/home-auth/home.auth.component.jsx
@@ -10,26 +10,74 @@ import {selectCurrentUser } from '../../store/user/user.selector'
 import Avatar from 'react-avatar';
 
 
+import { client } from "../../utils/axios/axios.utils";
+import { useState } from 'react'
 
 const HomeAuth = () => {
+    
     const dispatch = useDispatch()
     const handlerLogout = () => dispatch(setIsAuthUser(false))
+    
     const currentUser = useSelector(selectCurrentUser)
-    console.log(currentUser)
+    
+    const [riskData, setRiskData] = useState([])
+
+    
+    const handlerRisks = () => {
+        client("api/valutech/get_risks", {
+            headers: { 'Authorization': `Token ${currentUser.token}`}
+        })
+        .then(data => {
+            console.log(data.data.dados);
+            setRiskData(data.data.dados)
+        })
+    }
+    
+    
     return (
-        <div className="flex justify-between w-full px-10">
+        <div className="flex flex-col w-full">
+            <div className="flex justify-between w-full px-10">
+                {
+                    currentUser && currentUser.image ?
+                    <Avatar name="Foo Bar" src={currentUser.image} round={true} size="75"/>
+                    : null
+                }
+                <h1 className="text-white text-3xl mt-10">
+                    LOGGED USER AREA
+                </h1>
+                    <Button onClick={handlerLogout} variant="contained" color="error" size="small"
+                    className="h-10 !pt-0 !px-4">
+                            Log Out
+                        </Button>
+            </div>
+            <div className="flex mt-10 justify-center">
             {
-                currentUser && currentUser.image ?
-                <Avatar name="Foo Bar" src={currentUser.image} round={true} size="75"/>
-                : null
+                !riskData.length > 0 ?
+            <Button onClick={handlerRisks} variant="contained" color="primary" size="small"
+                    className="h-10 !pt-0 !px-4">
+                            Get Risks
+                        </Button>
+                    :
+            <Button onClick={() => setRiskData([])} variant="contained" color="warning" size="small"
+                    className="h-10 !pt-0 !px-4">
+                            Clear Risks
+                        </Button>
+
             }
-            <h1 className="text-white text-3xl mt-10">
-                LOGGED USER AREA
-            </h1>
-                <Button onClick={handlerLogout} variant="contained" color="error" size="small"
-                className="h-10 !pt-0 !px-4">
-						Log Out
-					</Button>
+            </div>
+            {riskData ? 
+            <div className="flex mt-10 justify-center max-h-[50vh] bg-gray-300 rounded-lg w-4/5 self-center overflow-scroll">
+                <ul>
+                {riskData.map((data, index) => {
+                    return (
+                            <li key={index}>Data: {data.x} - Valor: {data.y}</li>
+                    )
+                })}
+                </ul>
+            </div>
+            : null
+            }
+
         </div>
     )
 }
--- src/utils/axios/axios.utils.js
+++ src/utils/axios/axios.utils.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const baseURL = process.env.NODE_ENV !== "production" ? "http://127.0.0.1:8000/" : "https://valutech.herokuapp.com/"
 
+
 export const client = axios.create({
   baseURL: baseURL,
   headers: {
